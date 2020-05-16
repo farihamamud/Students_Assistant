00f684a40e73e9993f538d51d6441a506a8f7f07
Fariha <fariha.mamud02@gmail.com>
2020-05-16 13:32:28 +0600

    deleted lib

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:students_assistant/CRInfo.dart';
 import 'package:students_assistant/Routine.dart';
