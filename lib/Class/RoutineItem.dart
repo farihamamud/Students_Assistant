b62b6dee0d79a47586a685d240ea967ef49cc9ce
Fariha <fariha.mamud02@gmail.com>
2020-05-16 12:42:44 +0600

    ...

--- lib/Class/RoutineItem.dart
+++ lib/Class/RoutineItem.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 class RoutineItem{
   String cname,ccode,tname,time,room;
   RoutineItem(String cname,String ccode,String tname,String time,String room){
